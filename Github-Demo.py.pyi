280cfe72154f3d046bce0e32098c4cfacafdf552
ZAINZINGER Harald <zai@htl.rennweg.at>
2025-09-08 11:52:07 +0200

    main() added

--- Github-Demo.py.pyi
+++ Github-Demo.py.pyi
@@ -1,1 +1,8 @@
 #  Copyright (c) 2025. Harald Zainzinger, HTL Rennweg
+
+def main():
+    print("This is a type hinting stub file for the Github-Demo module.")
+    print("It contains no executable code.")
+
+if __name__ == "__main__":
+    main()
