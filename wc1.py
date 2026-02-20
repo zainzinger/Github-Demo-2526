57af764a038537657536d220c6ed4c822746c29f
ZAINZINGER Harald <zai@htl.rennweg.at>
2026-02-20 12:37:03 +0100

    Code verunstaltet, um black zu testen.

--- wc1.py
+++ wc1.py
@@ -1,11 +1,11 @@
 #  Copyright (c) 2026. Harald Zainzinger, HTL Rennweg
-import doctest
+# import doctest
 
 
-def count_lines(text):
+def      count_lines(   text  : str  ) -> int:
     """
     >>> count_lines('a \n b \n c')
-    3
+    4
     """
     return len(text.split("\n"))
 
