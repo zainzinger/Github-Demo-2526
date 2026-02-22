69d912d7209e666f5e82f2f1e5d4fc374af9b3e3
ZAINZINGER Harald <zai@htl.rennweg.at>
2026-02-22 16:14:20 +0100

    Update yml

--- wc1.py
+++ wc1.py
@@ -1,14 +1,14 @@
 #  Copyright (c) 2026. Harald Zainzinger, HTL Rennweg
-# import doctest
+import doctest
 
 
-def      count_lines(   text  : str  ) -> int:
+def count_lines(text):
     """
-    >>> print(2 + 1)
+    >>> count_lines('a \n b \n c')
     3
     """
     return len(text.split("\n"))
 
-if __name__ == '__main__':
 
-    print(count_lines('a \n b \n c'))
+if __name__ == "__main__":
+    print(count_lines("a \n b \n c"))
