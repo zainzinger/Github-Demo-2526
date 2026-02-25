24639e7531d8b806d9d51141920f1a9346c1523a
ZAINZINGER Harald <zai@htl.rennweg.at>
2026-02-25 14:38:14 +0100

    Demo in der 4xI am 20260225 1. Versuch

--- cicd-demo.py
+++ cicd-demo.py
@@ -1,5 +1,4 @@
 #  Copyright (c) 2026. Harald Zainzinger, HTL Rennweg
-
 """
 Demonstriere was in der Github CI/CD-Pipeline passiert, wenn
 * ein nicht benötigter import vorhanden ist
@@ -15,8 +14,12 @@ def count_lines( text ):
     >>> count_lines('a  b c')
     4
     """
-    return len(text.split())
+    return len(
+        text.split()
+    )
 
 
 if __name__ == "__main__":
+
+
     print(count_lines("a   b   c"))
